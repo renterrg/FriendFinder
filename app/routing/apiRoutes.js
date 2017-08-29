dd1d7928f0a6503543f92c6dbc8aae3e4e6b7215
Renzo Renteria <Renzo@Renzos-MacBook-Pro.local>
2017-08-29 17:46:37 -0400

    updated

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,9 +7,43 @@ module.exports = function(app) {
 	});
 
 	app.post("/api/friends", function(req, res){
-		console.log(req.body);
-		friendsData.push(req.body);
-		res.end();
+		
+		var user = req.body;
+
+		for (var i = 0; i < user.scores.length; i++) {
+			user.scores[i] = parseInt(user.scores[i]);
+		}
+
+
+				var diff = [];
+
+				for (var i = 0; i < friendsData.length; i++) {
+					var goodFriend = friendsData[i];
+
+					var totalDiff = 0;
+
+					for (var j = 0; j < goodFriend.scores.length; j++) {
+						var balance = Math.abs(goodFriend.scores[j] - user.scores[j]);
+						totalDiff += balance
+					}
+
+					diff[i] = totalDiff;
+				}
+
+
+						var diffUser = diff[0];
+						var bestMatch = 0;
+
+						for (var i = 0; i < diff.length; i++) {
+							if (diff[i] < diffUser) {
+								diffUser = diff[i];
+								bestMatch = i;
+							}
+						}
+
+		friendsData.push(user);
+
+		res.json(friendsData[bestMatch]);
 	});
 
 };
